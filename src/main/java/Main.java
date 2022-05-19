81b7ef0aca439851703104ea924a38a83c45237b
Timur Kondratyev <timurioo@Timurs-MacBook-Pro.local>
2022-05-19 22:17:48 +0300

    changing package

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-package redis.codecrafters;
-
 import java.io.IOException;
 import java.net.ServerSocket;
 
--- src/main/java/RespParser.java
+++ src/main/java/RespParser.java
@@ -1,5 +1,3 @@
-package redis.codecrafters;
-
 import java.util.ArrayList;
 import java.util.List;
 
